48b6dc810347c16df58328911b60850d65d517d1
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-25 13:42:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function validateform() {
+	
+var firstname=document.getElementById("fname").value;
+var lastname=document.getElementById("lname").value;
+var phonenum=document.getElementById("phone").value;
+var emailid=document.getElementById("email").value;
+
+var alertMsg="First Name: "+firstname+"Last Name: "+lastname+"Phone Number: "+phonenum+"Email Id: "+emailid;
+alert(alertMsg);
+}
+
